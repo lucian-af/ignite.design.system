5733f403aa78b4cdddce4224bb4356fe4d61c88d
Lucian AF <lucian_af@hotmail.com>
2023-10-31 21:24:40 -0300

    feat(docs): adiciona componente TextInput;

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,44 @@
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Forms/Text Input',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+            width: '450px',
+          }}
+        >
+          <Text size="sm"> E-mail</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Digite aqui ...',
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
