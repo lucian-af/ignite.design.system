e28359f782b531ca7b32a89b80fec5917f2ca67f
Lucian AF <lucian_af@hotmail.com>
2023-10-31 21:35:07 -0300

    feat(docs): adiciona componente TextArea;

---
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,38 @@
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Forms/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+            width: '450px',
+          }}
+        >
+          <Text size="sm">Observações</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Digite aqui ...',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
