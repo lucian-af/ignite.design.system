2165f0678e39cbf7f661015a6db06b55145a40f0
Lucian AF <lucian_af@hotmail.com>
2023-10-31 22:26:35 -0300

    feat(docs): adiciona componente Checkbox;

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,27 @@
+import { Text, Box, Checkbox, CheckboxProps } from '@ignite-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '$2',
+          }}
+        >
+          {Story()}
+          <Text size="sm">Marcar/Desmarcar</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Default: StoryObj<CheckboxProps> = {}
